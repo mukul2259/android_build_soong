3c9caf09de030f6fdeaef9c584965760996a03b2
Simon Shields <simon@lineageos.org>
2020-05-20 09:08:23 +0200

    soong: extend with 404 board flags
    Includes the following commit:
    
    Author: Simon Shields <simon@lineageos.org>
    Date:   Mon Sep 4 21:09:44 2017 +1000
    
        soong: explicitly specify name of 404 variables struct
    
        Change-Id: Ic754b8a13658ddc40620f985581bd1754b780b15
    
    Change-Id: If127c9da6b57340ae57e0cf725151cdcc3c82ef3
    Signed-off-by: Udit Karode <udit.karode@gmail.com>

--- android/variable.go
+++ android/variable.go
@@ -20,6 +20,8 @@ import (
 	"runtime"
 	"strings"
 
+	"p404/soong/android"
+
 	"github.com/google/blueprint/proptools"
 )
 
@@ -124,6 +126,8 @@ type variableProperties struct {
 			Static_libs  []string
 			Srcs         []string
 		}
+		// include p404 variables
+		P404 android.Product_variables
 	} `android:"arch_variant"`
 }
 
@@ -287,6 +291,9 @@ type productVariables struct {
 	ProductHiddenAPIStubsTest   []string `json:",omitempty"`
 
 	TargetFSConfigGen []string `json:",omitempty"`
+
+	// include p404 variables
+	P404 android.ProductVariables
 }
 
 func boolPtr(v bool) *bool {
